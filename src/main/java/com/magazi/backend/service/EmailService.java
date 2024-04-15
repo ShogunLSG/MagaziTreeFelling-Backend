f2e08dd5fe9818ab408345b2a5f73424b48f9b25
Lesego Matsimela <Lesego.Matsimela@rmb.co.za>
2024-04-15 15:11:34 +0200

    Hidding SMTP details (but properly this time)

--- src/main/java/com/magazi/backend/service/EmailService.java
+++ src/main/java/com/magazi/backend/service/EmailService.java
@@ -3,24 +3,33 @@ package com.magazi.backend.service;
 import com.magazi.backend.requestEntity.QuoteRequest;
 import jakarta.mail.internet.InternetAddress;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
-    private final String reciever = "mmatismbale@gmail.com";
 
     @Autowired
     public JavaMailSender mailSender;
 
+    @Value("${SPRING_MAIL_USERNAME}")
+    private String username;
+
+    @Value("${SPRING_MAIL_PASSWORD}")
+    private String password;
+
+    @Value("${SPRING_MAIL_RECIPIENT}")
+    private String receiver;
+
     public void sendEmail(QuoteRequest request) throws UnsupportedEncodingException {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(reciever);
-        mailMessage.setFrom(String.valueOf(new InternetAddress("mmatismbale@outlook.com","Magazi Tree Felling Mailer")));
+
+        mailMessage.setTo(receiver);
+        mailMessage.setFrom(String.valueOf(new InternetAddress(username,"Magazi Tree Felling Mailer")));
         mailMessage.setSubject("Quote Request from: "+ request.getNames() + " for " + request.getService() );
         mailMessage.setText("Good Day, \n  You have recieved a quote request for " +
                 request.getService() + ". This service is requested from the "+ request.getAddress() + " Area. "
